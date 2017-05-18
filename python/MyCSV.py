fac5e74c1dc0c77732b60f2cd49350d92e153b3d
chaowyc <chaowyc@gmail.com>
2017-05-18 20:18:48 +0800

    ..

--- python/MyCSV.py
+++ python/MyCSV.py
@@ -48,6 +48,7 @@ class MyCSV():
                             files_stack.push(tmp_file + os.sep + item)
                 else:
                     self.file_nums += 1
+                    print self.file_nums
                     self.PutTogether(tmp_file)
 
     def PutTogether(self, tmp_file_path):
